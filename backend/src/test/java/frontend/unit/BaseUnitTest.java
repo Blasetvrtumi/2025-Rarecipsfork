4cb09028e93500f114888f1b0e7880a1d1308e4c
Blas Vita Ramos <117302429+Blasetvrtumi@users.noreply.github.com>
2026-02-21 16:53:49 +0100

    Enable acceptance of insecure certificates in tests

--- backend/src/test/java/frontend/unit/BaseUnitTest.java
+++ backend/src/test/java/frontend/unit/BaseUnitTest.java
@@ -23,6 +23,7 @@ public class BaseUnitTest {
     @BeforeEach
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
+        options.setAcceptInsecureCerts(true);
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
